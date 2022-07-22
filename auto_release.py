4b81422b5b7784edb787206132da8518c7ea1e77
Hongpei Zheng <zhenghongpei@gmail.com>
2022-07-22 15:31:18 +0100

    CI update

--- auto_release.py
+++ auto_release.py
@@ -127,4 +127,5 @@ if __name__ == "__main__":
     if en_file.exists():
         en_global: LocalizationFile = LocalizationFile(en_file)
         en_global.save(release_path / f'[en][{to_version.replace("_", ".")}]global.ini')
+    print(to_version.replace("_", "."))
 
